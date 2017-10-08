bc16a01cb8d31a12090a7d042f76e630b84e14b3
Brandon <brandonbsuen@gmail.com>
2017-10-08 15:28:37 -0700

    generator-license incorporated

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,7 +1,6 @@
 'use strict';
 const Generator = require('yeoman-generator');
 const chalk = require('chalk');
-const username = require('username');
 const yosay = require('yosay');
 
 module.exports = class extends Generator {
@@ -10,10 +9,12 @@ module.exports = class extends Generator {
 
     super(args, opts);
 
-    const uname = username.sync();
-
     this.option('name', {
       default: this.appname,
+      hide: true,
+      type: String
+    });
+    this.option('username', {
       hide: true,
       type: String
     });
@@ -23,31 +24,26 @@ module.exports = class extends Generator {
       hide: true,
       type: String
     });
-    this.option('repository', {
+    this.option('homepage', {
       alias: 'rp',
-      default: '',
       hide: true,
       type: String
     });
     this.option('license', {
       alias: 'lcs',
-      default: 'MIT',
       hide: true,
       type: String
     });
     this.option('author', {
       alias: 'auth',
-      default: '',
       hide: true,
       type: String
     });
     this.option('email', {
-      default: '',
       hide: true,
       type: String
     });
     this.option('url', {
-      default: '',
       hide: true,
       type: String
     });
@@ -130,13 +126,13 @@ module.exports = class extends Generator {
           default: this.options.main,
           message: str + 'entry point?',
           name: 'main',
-          store: true,
+          //store: true,
           type: 'input'
         },
         {
-          default: this.options.repository,
-          message: str + 'repository url?',
-          name: 'repository',
+          default: this.options.homepage,
+          message: 'What\'s the url of your project\'s homepage?',
+          name: 'homepage',
           store: false,
           type: 'input'
         },
@@ -144,59 +140,39 @@ module.exports = class extends Generator {
           default: this.options.author,
           message: 'What\'s your name?',
           name: 'author',
-          store: true,
+          //store: true,
           type: 'input'
         },
         {
           default: this.options.email,
           message: 'What\'s your email?',
           name: 'email',
-          store: true,
+          //store: true,
           type: 'input'
         },
         {
           default: this.options.url,
-          message: 'What\'s the url for your personal website?',
+          message: 'What\'s the url for your personal website, Github profile, or some page about you?',
           name: 'url',
-          store: true,
+          //store: true,
           type: 'input'
-        },
-        {
-          choices: [
-            'Apache 2.0',
-            'BSD 2-Clause (FreeBSD) License',
-            'BSD 3-Clause (NewBSD) License',
-            'GNU AGPL 3.0',
-            'GNU GPL 3.0',
-            'GNU LGPL 3.0',
-            'Internet Systems Consortium (ISC) License',
-            'MIT',
-            'Mozilla Public License 2.0',
-            'No License (Copyrighted)',
-            'Unlicense'
-          ],
-          default: this.options.license,
-          message: str + 'license?',
-          name: 'license',
-          store: true,
-          type: 'list'
         },
         {
           default: this.options.private,
           message: 'Is your project private?',
           name: 'private',
-          store: true,
+          //store: true,
           type: 'confirm'
         },
         {
-          default: typeof this.options.keywords === 'undefined' ? this.options.keywords : undefined,
+          default: this.options.keywords,
           message: plural + 'keywords' + fyp + '(Example: keyword1, keyword2)',
           name: 'keywords',
           store: false,
           type: 'input'
         },
         {
-          default: typeof this.options.scripts === 'undefined' ? this.options.scripts : undefined,
+          default: this.options.scripts,
           message: plural + 'scripts' + fyp + '(Example: commands: command1 && comand2, execute: execution1 && execution2)',
           name: 'scripts',
           store: false,
@@ -212,47 +188,58 @@ module.exports = class extends Generator {
 
   writing() {
 
+    this.composeWith(require.resolve('generator-license'), {
+      name: this.options.name,
+      email: this.options.email,
+      website: this.options.url,
+      licensePrompt: 'Which license do you want to use?',
+      defaultLicense: this.options.license
+    });
+
     const pkg = {
       'name': this.options.name,
       'version': '0.0.0',
       'description': this.options.description,
       'main': this.options.main,
-      'repository': this.options.repository,
       'license': this.options.license,
-      'private': this.options.private,
-      'author': {
-        'name': this.options.author,
-        'email': this.options.email,
-        'url': this.options.url
-      }
+      'private': this.options.private
     };
-    if (typeof this.options.keywords === 'String') {
+    if (typeof this.options.homepage === 'string' && this.options.homepage.length > 0) {
+      pkg['homepage'] = this.options.homepage;
+    }
+    if ((typeof this.options.author === 'string' && this.options.author.length > 0) || (typeof this.options.email === 'string' && this.options.email.length > 0) || (typeof this.options.url === 'string' && this.options.url.length > 0)) {
+      pkg['author'] = new Object();
+    }
+    if (typeof this.options.author === 'string' && this.options.author.length > 0) {
+      pkg['author']['name'] = this.options.author;
+    }
+    if (typeof this.options.email === 'string' && this.options.email.length > 0) {
+      pkg['author']['email'] = this.options.email;
+    }
+    if (typeof this.options.url === 'string' && this.options.url.length > 0) {
+      pkg['author']['url'] = this.options.url;
+    }
+    if (typeof this.options.keywords === 'string' && this.options.scripts.length > 0) {
       const keywords = this.options.keywords.replace(/ /g, '').split(',');
       pkg['keywords'] = keywords;
     }
-    if (typeof this.options.scripts === 'String') {
+    if (typeof this.options.scripts === 'string' && this.options.scripts.length > 0) {
+      this.log(this.options.scripts);
+      this.log(typeof this.options.scripts);
       pkg['scripts'] = new Object();
       const scripts = this.options.scripts.split(',');
       let command;
       for (let q = 0; q < scripts.length; q++) {
+        this.log(scripts[q]);
+        this.log(scripts[q].split(':'));
         command = scripts[q].split(':');
-        if (command[1][0] == ' ') {
+        if (command[1].startsWith(' ')) {
           command[1] = command[1].substring(1);
         }
         pkg['scripts'][command[0].replace(/ /g, '')] = command[1];
       }
     }
     this.fs.writeJSON('./package.json', pkg);
-
-    /*this.composeWith(require.resolve('generator-license'), {
-      name: 'John Doe', // (optional) Owner's name
-      email: 'john.doe@example.com', // (optional) Owner's email
-      website: 'https://example.com', // (optional) Owner's website
-      year: '1945', // (optional) License year (defaults to current year)
-      licensePrompt: 'Which license do you want to use?' // (optional) customize license prompt text
-      defaultLicense: 'MIT', // (optional) Select a default license
-      license: 'MIT', // (optional) Select a license, so no license prompt will happen, in case you want to handle it outside of this generator
-    });*/
 
   }
 
